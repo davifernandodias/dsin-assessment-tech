25b7c3cfa417abb60426c0719d165fdc78d57353
Davi Dias <davifernandosousadias18@gmail.com>
2025-03-16 21:27:24 -0300

    feat(ui): added components/ui generetion

--- dashboard/apps/web/src/app/(auth)/page.tsx
+++ dashboard/apps/web/src/app/(auth)/page.tsx
@@ -1,11 +1,12 @@
 import Header from "@/components/header";
+import ScheduleInitial from "@/container/schedule-initial";
 
 export default function HomePage() {
   return (
     <section className="w-full">
-      <div className="w-full">
+      <div className="flex flex-col gap-16 px-16 py-10">
       <Header />
-      <div>oiii</div>
+      <ScheduleInitial />
       </div>
     </section>
   );
